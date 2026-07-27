2fd00763c814904e83b6912e2c20b51a1bd8cef3
Faizan    Arif <faizanarifgfx@gmail.com>
2026-07-27 23:20:50 +0500

    refactor: remove Google AdSense publisher script and clean ads.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,16 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="google-adsense-account" content="ca-pub-1701881740445095" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1701881740445095"
-          crossOrigin="anonymous"
-        />
-        <link rel="preconnect" href="https://pagead2.googlesyndication.com" crossOrigin="anonymous" />
-        <link rel="preconnect" href="https://googleads.g.doubleclick.net" crossOrigin="anonymous" />
-      </head>
+      <head />
       <body className={inter.variable}>
         <Header />
 
